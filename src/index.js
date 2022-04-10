698ce48c7eddd451da4e2afd6dc25622e09494aa
tinalin923 <tinalin923@gmail.com>
2022-04-11 01:57:42 +0800

    figure out the webpack.config.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+/* eslint-disable import/extensions */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/main.scss';
+import App from './App.js';
+
+// eslint-disable-next-line react/jsx-filename-extension
+ReactDOM.render(<App />, document.getElementById('root'));
+// eslint-disable-next-line no-console
+console.log('hello webpack');
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        options: { presets: ['@babel/preset-env'] },
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      inject: 'body',
+      title: 'NeighbourHood',
+      filename: 'index.html',
+      template: 'src/template.html',
+    }),
+  ],
+  watch: true,
+};
