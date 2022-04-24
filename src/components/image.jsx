86787f93fe2cf30728eeb8dbe596dc315ab6c72b
tinalin923 <tinalin923@gmail.com>
2022-04-25 07:19:42 +0800

    try to get data from firestore

--- src/components/image.jsx
+++ src/components/image.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
+import useFirestore from '../hooks/useFirestore.js';
 
-const UploadImage = () => <div>images</div>;
-
+const UploadImage = () => {
+  const { docs } = useFirestore('covers');
+  console.log(docs);
+  return <div>images</div>;
+};
 export default UploadImage;
--- src/index.jsx
+++ src/index.jsx
@@ -7,8 +7,7 @@ import UploadImage from './components/image.jsx';
 
 import './styles/style.scss';
 
-ReactDOM.render(<UploadImage />, document.getElementById('neighbour_cover'));
 ReactDOM.render(<UploadForm />, document.getElementById('input_file_btn'));
-ReactDOM.render(<Btn />, document.getElementById('add_block_button'));
 ReactDOM.render(<Switch />, document.getElementById('toggle_preview'));
-// ReactDOM.render(<App />, document.getElementsByTagName('h1')[0]);
+ReactDOM.render(<UploadImage />, document.getElementById('neighbour_cover'));
+ReactDOM.render(<Btn />, document.getElementById('add_block_button'));
