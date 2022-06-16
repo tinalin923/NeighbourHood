ce9e3cf64d27c7f66335caf2d390e7ca23d18564
tinalin923 <tinalin923@gmail.com>
2022-06-16 23:57:37 +0800

    create editItemReducer file

---
+++ src/components/contexts/editItemReducer.js
@@ -0,0 +1,29 @@
+export const initialEditState = {
+  temporaryUrl: '',
+  imageError: '',
+  id: '',
+  title: '',
+  details: '',
+};
+
+const editItemReducer = (state, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case 'SET_TEMPORARY_URL':
+      return { ...state, temporaryUrl: payload.temporaryUrl };
+    case 'SET_IMAGE_ERROR':
+      return { ...state, imageError: payload.imageError };
+
+    case 'SET_ID':
+      return { ...state, id: payload.id };
+    case 'SET_TITLE':
+      return { ...state, title: payload.title };
+    case 'SET_DETAILS':
+      return { ...state, details: payload.details };
+
+    default:
+      throw new Error();
+  }
+};
+
+export default editItemReducer;
