935a3dc7e963d49af7a8925592e5173bdb81e17a
tinalin923 <tinalin923@gmail.com>
2022-06-21 00:55:20 +0800

    Revert "update rules of eslint & prettier"
    This reverts commit 844f63952cde6402184345e8639be79a9b5773d8.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,18 +2,21 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true,
   },
-  extends: ["plugin:prettier/recommended", "prettier", "eslint:recommended"],
-  parser: "@babel/eslint-parser",
+  extends: [
+    'airbnb',
+    'plugin:jsx-a11y/recommended',
+    'prettier',
+    'eslint:recommended',
+  ],
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  plugins: ["prettier", "react", "react-hooks"],
+  plugins: ['prettier', 'react', 'jsx-a11y', 'react-hooks'],
   rules: {
-    "prettier/prettier": [
-      "error",
+    'prettier/prettier': [
+      'error',
       {},
       {
         fileInfoOptions: {
@@ -21,34 +24,35 @@ module.exports = {
         },
       },
     ],
-    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
-    "react/prefer-stateless-function": [1, { ignorePureComponents: true }],
-    "react/function-component-definition": [
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'import/extensions': [1, 'ignorePackages'],
+    'react/prefer-stateless-function': [1, { ignorePureComponents: true }],
+    'react/function-component-definition': [
       1,
       {
         namedComponents: [
-          "function-declaration",
-          "function-expression",
-          "arrow-function",
+          'function-declaration',
+          'function-expression',
+          'arrow-function',
         ],
-        unnamedComponents: "arrow-function",
+        unnamedComponents: 'arrow-function',
       },
     ],
-    "function-paren-newline": ["error", "multiline-arguments"],
-    "react/jsx-one-expression-per-line": [0, { allow: "single-child" }],
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn",
-    "max-classes-per-file": ["error", 3],
-    "no-console": "off",
-    "react/forbid-prop-types": [
+    'function-paren-newline': ['error', 'multiline-arguments'],
+    'react/jsx-one-expression-per-line': [0, { allow: 'single-child' }],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'max-classes-per-file': ['error', 3],
+    'no-console': 'off',
+    'react/forbid-prop-types': [
       1,
       {
-        forbid: ["any"],
+        forbid: ['any'],
         checkContextTypes: false,
         checkChildContextTypes: false,
       },
     ],
-    "react/jsx-no-bind": [
+    'react/jsx-no-bind': [
       1,
       {
         ignoreDOMComponents: true,
