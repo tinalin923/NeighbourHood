018a7f17621264f9c9f164d6dbb4123249f9d86d
tinalin923 <tinalin923@gmail.com>
2022-06-21 19:08:11 +0800

    change firebase rules & delete analytic

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,5 +1,4 @@
 import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
 // auth
 import { getAuth } from 'firebase/auth';
 // import { connectAuthEmulator, getAuth } from 'firebase/auth';
@@ -24,7 +23,6 @@ const firebaseConfig = {
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 const auth = getAuth(app);
 
@@ -44,5 +42,4 @@ export {
   usersCollection,
   villageCollection,
   projectStorage,
-  analytics,
 };
